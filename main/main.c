4a179e57474d303c35f58c0fd0b84cff0a67469f
fabiokleis <fabiohkrc@gmail.com>
2024-08-10 17:31:39 -0300

    tentando fazer o lcd touch driver funcionar

--- main/main.c
+++ main/main.c
@@ -7,73 +7,75 @@
 #include "esp_check.h"
 #include "esp_timer.h"
 #include "driver/gpio.h"
+#include "driver/spi_master.h"
 #include "lvgl.h"
 #include "esp_lvgl_port.h"
-#include "driver/spi_master.h"
-#include "freertos/semphr.h"
+#include "esp_lcd_touch.h"
 #include "esp_lcd_ili9341.h"
+#include "esp_lcd_touch_xpt2046.h"
 
-#define LCD_HOST               SPI2_HOST
+/* LCD settings */
+#define LCD_HOST               (SPI2_HOST)
 #define LCD_H_RES              (320)
 #define LCD_V_RES              (240)
 #define LCD_BIT_PER_PIXEL      (16)
 
+/* LCD pins */
 #define PIN_NUM_LCD_CS         (GPIO_NUM_15)
 #define PIN_NUM_LCD_PCLK       (GPIO_NUM_18)
-// mosi
+/* MOSI */
 #define PIN_NUM_LCD_DATA0      (GPIO_NUM_23) 
 #define PIN_NUM_LCD_DC         (GPIO_NUM_2)
 #define PIN_NUM_LCD_RST        (GPIO_NUM_4)
-#define PIN_NUM_BK_LIGHT       (GPIO_NUM_2)
+/* LED */
+#define PIN_NUM_BK_LIGHT       (GPIO_NUM_2) 
+
+/* LCD touch pins */
+#define PIN_NUM_TOUCH_CS           (GPIO_NUM_27)
+#define PIN_NUM_TOUCH_INT          (GPIO_NUM_26)
+#define PIN_NUM_TOUCH_SPI_MISO     (GPIO_NUM_13)
+
 
 static const char *TAG = "esp-hello-world";
 static esp_lcd_panel_handle_t lcd_panel = NULL;
 static esp_lcd_panel_io_handle_t io_handle = NULL;
+static esp_lcd_touch_handle_t touch_handle = NULL;
+
+/* lvgl */
 static lv_display_t *lvgl_disp = NULL;
-/* static SemaphoreHandle_t refresh_finish = NULL; */
+static lv_indev_t *lvgl_touch_indev = NULL;
 
-/* IRAM_ATTR static bool test_notify_refresh_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx) */
-/* { */
-/*     BaseType_t need_yield = pdFALSE; */
-
-/*     xSemaphoreGiveFromISR(refresh_finish, &need_yield); */
-/*     return (need_yield == pdTRUE); */
-/* } */
-
-
-/* static void draw_bitmap(esp_lcd_panel_handle_t panel_handle) */
-/* { */
-/*     refresh_finish = xSemaphoreCreateBinary(); */
-
-/*     uint16_t row_line = LCD_V_RES / LCD_BIT_PER_PIXEL; */
-/*     uint8_t byte_per_pixel = LCD_BIT_PER_PIXEL / 8; */
-/*     uint8_t *color = (uint8_t *)heap_caps_calloc(1, row_line * LCD_H_RES * byte_per_pixel, MALLOC_CAP_DMA); */
-
-/*     for (int j = 0; j < LCD_BIT_PER_PIXEL; j++) { */
-/*         for (int i = 0; i < row_line * LCD_H_RES; i++) { */
-/*             for (int k = 0; k < byte_per_pixel; k++) { */
-/*                 color[i * byte_per_pixel + k] = (SPI_SWAP_DATA_TX(BIT(j), LCD_BIT_PER_PIXEL) >> (k * 8)) & 0xff; */
-/*             } */
-/*         } */
-/*         ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, 0, j * row_line, LCD_H_RES, (j + 1) * row_line, color)); */
-/*         xSemaphoreTake(refresh_finish, portMAX_DELAY); */
-/*     } */
-/*     free(color); */
-/*     vSemaphoreDelete(refresh_finish); */
-/* } */
 
 static esp_err_t init_lcd() {
     esp_err_t ret = ESP_OK;
 
     ESP_LOGI(TAG, "Initialize SPI bus");
-    const spi_bus_config_t bus_config = ILI9341_PANEL_BUS_SPI_CONFIG(PIN_NUM_LCD_PCLK, PIN_NUM_LCD_DATA0,
-                                                                     LCD_H_RES * 80 * (LCD_BIT_PER_PIXEL / 2));
+
+    const spi_bus_config_t bus_config = {                                                           
+        .sclk_io_num = PIN_NUM_LCD_PCLK,                                    
+        .mosi_io_num = PIN_NUM_LCD_DATA0,                                    
+        .miso_io_num = PIN_NUM_TOUCH_SPI_MISO,                                      
+        .quadhd_io_num = -1,                                    
+        .quadwp_io_num = -1,                                    
+        .max_transfer_sz = LCD_H_RES * 80 * (LCD_BIT_PER_PIXEL / 2),                        
+    };
+    
     ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &bus_config, SPI_DMA_CH_AUTO));
 
     ESP_LOGI(TAG, "Install panel IO");
 
-    const esp_lcd_panel_io_spi_config_t io_config = ILI9341_PANEL_IO_SPI_CONFIG(PIN_NUM_LCD_CS, PIN_NUM_LCD_DC,
-                                                                                NULL, NULL);
+    const esp_lcd_panel_io_spi_config_t io_config = {                                                               
+        .cs_gpio_num = PIN_NUM_LCD_CS,                                          
+        .dc_gpio_num = PIN_NUM_LCD_DC,                                          
+        .spi_mode = 0,                                              
+        .pclk_hz = 40 * 1000 * 1000,                                
+        .trans_queue_depth = 10,                                    
+        .on_color_trans_done = NULL,                            
+        .user_ctx = NULL,                                   
+        .lcd_cmd_bits = 8,                                          
+        .lcd_param_bits = 8,                                        
+    };
+    
     ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
     ESP_LOGI(TAG, "Install ili9341 panel driver");
     const esp_lcd_panel_dev_config_t panel_config = {
@@ -88,8 +90,29 @@ static esp_err_t init_lcd() {
     ESP_ERROR_CHECK(esp_lcd_panel_mirror(lcd_panel, true, true));
     ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(lcd_panel, true));
     
-    ESP_LOGI(TAG, "Installed ili9341 panel driver");  
+    ESP_LOGI(TAG, "Installed ili9341 panel driver");    
 
+
+    ESP_LOGI(TAG, "Install XPT2046 touch controller");    
+    esp_lcd_panel_io_handle_t touch_io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t touch_io_config = ESP_LCD_TOUCH_IO_SPI_XPT2046_CONFIG(PIN_NUM_TOUCH_CS);
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &touch_io_config, &touch_io_handle));
+
+    esp_lcd_touch_config_t touch_config = {
+        .x_max = LCD_H_RES,
+        .y_max = LCD_V_RES,
+        .rst_gpio_num = -1,
+        .int_gpio_num = PIN_NUM_TOUCH_INT,
+        .flags = {
+            .swap_xy = true,
+            .mirror_x = true,
+            .mirror_y = true,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_touch_new_spi_xpt2046(touch_io_handle, &touch_config, &touch_handle));
+    ESP_LOGI(TAG, "Installed touch controller XPT2046");
+    
     return ret;
 }
 
@@ -128,7 +151,50 @@ static esp_err_t app_lvgl_init(void)
     };
     lvgl_disp = lvgl_port_add_disp(&disp_cfg);
 
+    const lvgl_port_touch_cfg_t touch_cfg = {
+        .disp = lvgl_disp,
+        .handle = touch_handle,
+    };
+    lvgl_touch_indev = lvgl_port_add_touch(&touch_cfg);
+
     return ESP_OK;
+}
+
+
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    ESP_LOGI(TAG, "chamou chamou %d\n", code);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
+void create_counter_button(lv_obj_t* scr)
+{
+    lv_obj_t * btn = lv_button_create(scr);     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+    lv_obj_center(label);
+}
+
+void create_hello_world(lv_obj_t* scr) {
+    lv_obj_t *label = lv_label_create(scr);
+    lv_obj_set_width(label, LCD_H_RES);
+    lv_obj_set_style_bg_color(scr, lv_color_hex(0xe1bdff), LV_PART_MAIN);
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
+    lv_label_set_text(label, "esp32 Hello world!");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, -30);
 }
 
 void app_main(void)
@@ -144,24 +210,17 @@ void app_main(void)
     ESP_ERROR_CHECK(init_lcd());
     // draw_bitmap(lcd_handle); /* test drawing */
     vTaskDelay(pdMS_TO_TICKS(1000));
+    ESP_ERROR_CHECK(gpio_set_level(PIN_NUM_BK_LIGHT, 0));
     ESP_LOGI(TAG, "Display Hello World!");
     ESP_ERROR_CHECK(app_lvgl_init());
 
-    /* Task lock */
     lvgl_port_lock(0);
     lv_obj_t *scr = lv_scr_act();
-    /* Label */
-    lv_obj_t *label = lv_label_create(scr);
-    lv_obj_set_width(label, LCD_H_RES);
-    lv_obj_set_style_bg_color(scr, lv_color_hex(0xe1bdff), LV_PART_MAIN);
-    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
-    lv_label_set_text(label, "esp32 Hello world!");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, -30);
-    
-    /* Task unlock */
+    create_hello_world(scr);
+    create_counter_button(scr);
     lvgl_port_unlock();
-
+    
     while (1) {
-      vTaskDelay(2000 / portTICK_PERIOD_MS);
+      vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
