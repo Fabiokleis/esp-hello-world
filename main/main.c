afacfe24ed1ab1af214e2310278085193c6a381e
fabiokleis <fabiohkrc@gmail.com>
2024-08-10 21:35:13 -0300

    driver touch instalad

--- main/main.c
+++ main/main.c
@@ -16,25 +16,24 @@
 
 /* LCD settings */
 #define LCD_HOST               (SPI2_HOST)
+#define LCD_TOUCH_HOST         (SPI3_HOST)
 #define LCD_H_RES              (320)
 #define LCD_V_RES              (240)
 #define LCD_BIT_PER_PIXEL      (16)
 
-/* LCD pins */
-#define PIN_NUM_LCD_CS         (GPIO_NUM_15)
-#define PIN_NUM_LCD_PCLK       (GPIO_NUM_18)
-/* MOSI */
-#define PIN_NUM_LCD_DATA0      (GPIO_NUM_23) 
-#define PIN_NUM_LCD_DC         (GPIO_NUM_2)
-#define PIN_NUM_LCD_RST        (GPIO_NUM_4)
-/* LED */
-#define PIN_NUM_BK_LIGHT       (GPIO_NUM_2) 
-
-/* LCD touch pins */
-#define PIN_NUM_TOUCH_CS           (GPIO_NUM_27)
-#define PIN_NUM_TOUCH_INT          (GPIO_NUM_26)
-#define PIN_NUM_TOUCH_SPI_MISO     (GPIO_NUM_13)
-
+/* pins */
+#define PIN_LCD_CS           (GPIO_NUM_15)
+#define PIN_LCD_RST          (GPIO_NUM_4)
+#define PIN_LCD_DC           (GPIO_NUM_2)
+#define PIN_LCD_MOSI         (GPIO_NUM_13)
+#define PIN_LCD_SCK          (GPIO_NUM_14)
+#define PIN_LCD_LED          (GPIO_NUM_21)
+#define PIN_LCD_MISO         (GPIO_NUM_12)
+#define PIN_LCD_TOUCH_CLK    (GPIO_NUM_25)
+#define PIN_LCD_TOUCH_CS     (GPIO_NUM_33)
+#define PIN_LCD_TOUCH_DIN    (GPIO_NUM_32)
+#define PIN_LCD_TOUCH_OUT    (GPIO_NUM_39)
+#define PIN_LCD_TOUCH_IRQ    (GPIO_NUM_36)
 
 static const char *TAG = "esp-hello-world";
 static esp_lcd_panel_handle_t lcd_panel = NULL;
@@ -46,40 +45,41 @@ static lv_display_t *lvgl_disp = NULL;
 static lv_indev_t *lvgl_touch_indev = NULL;
 
 
+
 static esp_err_t init_lcd() {
     esp_err_t ret = ESP_OK;
 
     ESP_LOGI(TAG, "Initialize SPI bus");
 
     const spi_bus_config_t bus_config = {                                                           
-        .sclk_io_num = PIN_NUM_LCD_PCLK,                                    
-        .mosi_io_num = PIN_NUM_LCD_DATA0,                                    
-        .miso_io_num = PIN_NUM_TOUCH_SPI_MISO,                                      
-        .quadhd_io_num = -1,                                    
-        .quadwp_io_num = -1,                                    
-        .max_transfer_sz = LCD_H_RES * 80 * (LCD_BIT_PER_PIXEL / 2),                        
+        .sclk_io_num = PIN_LCD_SCK,
+        .mosi_io_num = PIN_LCD_MOSI,
+        .miso_io_num = PIN_LCD_MISO,
+        .quadhd_io_num = -1,
+        .quadwp_io_num = -1,
+        .max_transfer_sz = LCD_H_RES * 80 * (LCD_BIT_PER_PIXEL / 2),
     };
     
     ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &bus_config, SPI_DMA_CH_AUTO));
 
     ESP_LOGI(TAG, "Install panel IO");
 
-    const esp_lcd_panel_io_spi_config_t io_config = {                                                               
-        .cs_gpio_num = PIN_NUM_LCD_CS,                                          
-        .dc_gpio_num = PIN_NUM_LCD_DC,                                          
-        .spi_mode = 0,                                              
-        .pclk_hz = 40 * 1000 * 1000,                                
-        .trans_queue_depth = 10,                                    
-        .on_color_trans_done = NULL,                            
-        .user_ctx = NULL,                                   
-        .lcd_cmd_bits = 8,                                          
-        .lcd_param_bits = 8,                                        
+    const esp_lcd_panel_io_spi_config_t io_config = {
+        .cs_gpio_num = PIN_LCD_CS,
+        .dc_gpio_num = PIN_LCD_DC,
+        .spi_mode = 0,
+        .pclk_hz = 40 * 1000 * 1000,
+        .trans_queue_depth = 10,
+        .on_color_trans_done = NULL,
+        .user_ctx = NULL,
+        .lcd_cmd_bits = 8,
+        .lcd_param_bits = 8,
     };
     
     ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
     ESP_LOGI(TAG, "Install ili9341 panel driver");
     const esp_lcd_panel_dev_config_t panel_config = {
-      .reset_gpio_num = PIN_NUM_LCD_RST,
+      .reset_gpio_num = PIN_LCD_RST,
       .rgb_endian = LCD_RGB_ENDIAN_BGR,
       .bits_per_pixel = LCD_BIT_PER_PIXEL,
     };
@@ -92,24 +92,59 @@ static esp_err_t init_lcd() {
     
     ESP_LOGI(TAG, "Installed ili9341 panel driver");    
 
-
+    
     ESP_LOGI(TAG, "Install XPT2046 touch controller");    
     esp_lcd_panel_io_handle_t touch_io_handle = NULL;
-    esp_lcd_panel_io_spi_config_t touch_io_config = ESP_LCD_TOUCH_IO_SPI_XPT2046_CONFIG(PIN_NUM_TOUCH_CS);
-    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &touch_io_config, &touch_io_handle));
+    esp_lcd_panel_io_spi_config_t touch_io_config = {                                                   
+        .cs_gpio_num = PIN_LCD_TOUCH_CS,
+        .dc_gpio_num = GPIO_NUM_NC,
+        .spi_mode = 0,
+        .pclk_hz = 2 * 1000 * 1000,// ESP_LCD_TOUCH_SPI_CLOCK_HZ
+        .trans_queue_depth = 3,
+        .on_color_trans_done = NULL,
+        .user_ctx = NULL,
+        .lcd_cmd_bits = 8,
+        .lcd_param_bits = 8,
+        .flags = {
+	  .dc_low_on_data = 0,
+	  .octal_mode = 0,
+	  .sio_mode = 0,
+	  .lsb_first = 0,
+	  .cs_high_active = 0
+	}
+    };
+
+    static const int SPI_MAX_TRANSFER_SIZE = 32768;
+    const spi_bus_config_t buscfg_touch = {
+      .mosi_io_num = PIN_LCD_TOUCH_DIN,
+      .miso_io_num = PIN_LCD_TOUCH_OUT,
+      .sclk_io_num = PIN_LCD_TOUCH_CLK,
+      .quadwp_io_num = GPIO_NUM_NC,
+      .quadhd_io_num = GPIO_NUM_NC,
+      .data4_io_num = GPIO_NUM_NC,
+      .data5_io_num = GPIO_NUM_NC,
+      .data6_io_num = GPIO_NUM_NC,
+      .data7_io_num = GPIO_NUM_NC,
+      .max_transfer_sz = SPI_MAX_TRANSFER_SIZE,
+      .flags = SPICOMMON_BUSFLAG_SCLK | SPICOMMON_BUSFLAG_MISO | SPICOMMON_BUSFLAG_MOSI | SPICOMMON_BUSFLAG_MASTER | SPICOMMON_BUSFLAG_GPIO_PINS,
+      .isr_cpu_id = INTR_CPU_ID_AUTO,
+      .intr_flags = ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_IRAM
+    };
 
     esp_lcd_touch_config_t touch_config = {
-        .x_max = LCD_H_RES,
-        .y_max = LCD_V_RES,
-        .rst_gpio_num = -1,
-        .int_gpio_num = PIN_NUM_TOUCH_INT,
-        .flags = {
+      .x_max = LCD_H_RES,
+      .y_max = LCD_V_RES,
+      .rst_gpio_num = PIN_LCD_RST,
+      .int_gpio_num = PIN_LCD_TOUCH_IRQ,
+      .flags = {
             .swap_xy = true,
             .mirror_x = true,
             .mirror_y = true,
         },
     };
-
+    
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_TOUCH_HOST, &buscfg_touch, SPI_DMA_CH_AUTO));
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_TOUCH_HOST, &touch_io_config, &touch_io_handle));
     ESP_ERROR_CHECK(esp_lcd_touch_new_spi_xpt2046(touch_io_handle, &touch_config, &touch_handle));
     ESP_LOGI(TAG, "Installed touch controller XPT2046");
     
@@ -201,16 +236,19 @@ void app_main(void)
 {
     printf("ESP32 Hello World!\n");
     /* LCD HW initialization */
-    gpio_config_t bk_gpio_config = {
+    gpio_config_t io_config = {
         .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = 1ULL << PIN_NUM_BK_LIGHT
+	.pin_bit_mask = (1ULL << PIN_LCD_LED)
     };
-    ESP_ERROR_CHECK(gpio_config(&bk_gpio_config));
-    ESP_ERROR_CHECK(gpio_set_level(PIN_NUM_BK_LIGHT, 1));
+    
+    ESP_ERROR_CHECK(gpio_config(&io_config));
+    ESP_ERROR_CHECK(gpio_set_level(PIN_LCD_LED, 1));
+    
+    
     ESP_ERROR_CHECK(init_lcd());
-    // draw_bitmap(lcd_handle); /* test drawing */
+    
     vTaskDelay(pdMS_TO_TICKS(1000));
-    ESP_ERROR_CHECK(gpio_set_level(PIN_NUM_BK_LIGHT, 0));
+    
     ESP_LOGI(TAG, "Display Hello World!");
     ESP_ERROR_CHECK(app_lvgl_init());
 
@@ -219,8 +257,21 @@ void app_main(void)
     create_hello_world(scr);
     create_counter_button(scr);
     lvgl_port_unlock();
+
+    ESP_ERROR_CHECK(esp_lcd_touch_read_data(touch_handle));
+    uint16_t x[1];
+    uint16_t y[1];
+    uint16_t  strength[1];
+    uint8_t count = 0;
+
+    bool touchpad_pressed = false; 
+
     
+    ESP_LOGI(TAG, "Minimum free heap size: %"PRIu32" bytes\n", esp_get_minimum_free_heap_size());
     while (1) {
+      ESP_ERROR_CHECK(esp_lcd_touch_read_data(touch_handle));
+      esp_lcd_touch_get_coordinates(touch_handle, x, y, strength, &count, 1);
       vTaskDelay(pdMS_TO_TICKS(1000));
+      ESP_LOGI(TAG, "counter: %d x: %d y: %d touchpad_pressed: %d", count, *x, *y, touchpad_pressed);
     }
 }
